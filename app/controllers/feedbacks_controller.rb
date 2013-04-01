fe6d3cc243cc26cd43b12d551d5b10703773c85e
Steve Hook <steve.hook@gmail.com>
2013-04-01 13:04:35 +0100

    Adds cancel button to the feedback forms with other refinements

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,25 +1,4 @@
 class FeedbacksController < ApplicationController
-  #def create
-    ##subscription = Subscription.new params[:subscription]
-    ##success = subscription.save
-    ##UserMailer.welcome_email(subscription).deliver if success
-    ##if request.xhr?
-      #@subscription = success ? Subscription.new : subscription
-      #if success
-        #@notice = "Thankyou, you are now subscribed to zanzaneet.com"
-      #else
-        #@error = "Your subscription could not be created. #{subscription.errors.full_messages.join(', ')}."
-      #end
-    ##else
-      ##if success
-        ##redirect_to root_path + '#contact', :notice => 'Thankyou, you are now subscribed to zanzaneet.com'
-      ##else
-        ##flash[:error] = "Your subscription could not be created. #{subscription.errors.full_messages.join(', ')}."
-        ##redirect_to root_path + '#contact'
-      ##end
-    ##end
-  #end
-
   def new
     @category = params[:category]
     @feedback = Feedback.new(category: @category)
