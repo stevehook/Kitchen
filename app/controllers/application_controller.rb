9f9123f8a1a9edc58f8856d95da7a20262e670ed
Steve Hook <steve.hook@gmail.com>
2013-04-29 15:20:02 +0100

    Remove http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with name: 'thechimni', password: 'I have fresh methi' if Rails.env.production?
 end
